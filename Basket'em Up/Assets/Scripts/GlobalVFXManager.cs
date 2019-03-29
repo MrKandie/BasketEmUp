00f35ada3dda6cb921f2a2b2317e66984e2ab2e7
BoxCollider <q.roze@rubika-edu.com>
2019-03-29 22:57:05 +0100

    MAde a script (GlobalVFXManager) that control Post-Process Runtime

---
+++ Basket'em Up/Assets/Scripts/GlobalVFXManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class GlobalVFXManager : MonoBehaviour
+{
+    PostProcessVolume myPP;
+    Bloom bloomLayer;
+    AmbientOcclusion ambientOcclusionLayer;
+    ColorGrading colorGradingLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myPP = GetComponent<PostProcessVolume>();
+        myPP.profile.TryGetSettings(out bloomLayer);
+        myPP.profile.TryGetSettings(out ambientOcclusionLayer);
+        myPP.profile.TryGetSettings(out colorGradingLayer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bloomLayer.intensity.value = Mathf.Sin(Time.time) * 50 + 50;
+    }
+}
