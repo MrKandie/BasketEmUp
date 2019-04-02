f2ef7db071ac859ba0632fcbb98c3346de6cc6d2
BoxCollider <q.roze@rubika-edu.com>
2019-04-02 17:40:53 +0200

    setting up narrative part

--- Basket'em Up/Assets/Scripts/NarrationPart/NarrationPlayerController.cs
+++ Basket'em Up/Assets/Scripts/NarrationPart/NarrationPlayerController.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 public class NarrationPlayerController : MonoBehaviour
 {
     public Animator myAnim;
+    public bool player1IsTrue;
+    Vector3 actuelDirection;
+    public Rigidbody rb;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        UpdatePosition();
+        UpdateRotation();
+    }
+
+    void UpdatePosition()
+    {
+        if (player1IsTrue)
         {
-            if (myAnim.GetFloat("Blend") > .25f)
-            {
-                myAnim.SetFloat("Blend", 0);
-            }
-            else
-            {
-                myAnim.SetFloat("Blend", .5f);
-            }
+            Vector3 inputVector1 = Input.GetAxis("Horizontal_1") * Camera.main.transform.right;
+            Vector3 inputVector2 = Input.GetAxis("Vertical_1") * Camera.main.transform.forward;
+            rb.velocity = Vector3.Lerp(rb.velocity, inputVector2 + inputVector1, 0.1f);
         }
+        else
+        {
+            Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal_1"), 0, Input.GetAxis("Vertical_1"));
+            rb.velocity = Vector3.Lerp(rb.velocity, inputVector, 0.1f);
+        }
+    }
+
+    void UpdateRotation()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(rb.velocity), 0.1f);
     }
 }
